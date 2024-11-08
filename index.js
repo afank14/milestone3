dbb7441c4c72615c4f4e4a77d0935b9bcc6adaa7
afank14 <103207126+afank14@users.noreply.github.com>
2024-11-07 21:53:54 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let express = require("express");
+let app = new express();
+app.set("view engine", "ejs");
+
+// Set up database connection using environment variables
+const knex = require("knex")({
+    client: "mysql",
+    connection: {
+        host: "milestone3-dbinstance-4lzf005ubyuz.cvm8gw8a626k.us-east-2.rds.amazonaws.com",
+        user: "admin",
+        password: "Milestone2!**",
+        database: "donut_db",
+        port: 3306,
+    },
+});
+
+app.get("/", (req, res) => {
+    knex
+        .select()
+        .from("donuts")
+        .then((result) => {
+            res.render("index", { aDonutList: result });
+        });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
